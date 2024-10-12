2f359c2ace87bd3a559a4e50eaff366a648abdf2
smallfu6 <smallfu666@gmail.com>
2024-10-12 23:20:53 +0800

    feat: add Footer page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import '../styles/Header.css'; 
+import '../styles/Header.css';
 
 const Header: React.FC = () => {
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/dashboard">Dashboard</Link>
-                    </li>
-                    {/* <li>
-                        <Link to="/about">About</Link>
-                    </li> */}
-                </ul>
-            </nav>
-        </header>
+        <div className="header-container">
+            <Link to="/dashboard">Dashboard</Link>
+        </div>
     );
 };
 
